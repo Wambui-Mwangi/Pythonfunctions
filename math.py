003e257e59af3af94fdb391b414f674d6a10e406
Esther <mwangiestherwambuii@gmail.com>
2023-03-31 15:44:32 +0300

    python functions

---
+++ math.py
@@ -0,0 +1,22 @@
+def add(a,b):
+    answer = a+b
+    return answer
+
+def subtract (c,d):
+    result = c-d
+    return result
+
+def divide(e,f):
+    quotient = e/f
+    return quotient
+
+def multiply(num1, num2):
+    product = num1*num2
+    return product
+
+def remainder(num3, num4):
+    modulus = num3%num4
+    return modulus
+
+
+
